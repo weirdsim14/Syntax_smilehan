9747b8aebca3c0d03e24820057183f30c9a30fc7
sim <sim30217@gmail.com>
2023-09-27 22:36:42 +0900

    object allocation

--- src/fn.cpp
+++ src/fn.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+
+class Dog {
+    private:
+        int age;
+    public:
+        int getAge();
+        void setAge(int a);
+};
+
+int Dog::getAge(){
+    return age;
+}
+void Dog::setAge(int a){
+    int age = a;
+}
 int main()
 {
-    int i;
-    std::cout << "how many ==";
-    std::cin >> i;
-    int num[i];
+    Dog *dp;
+    dp = new Dog;
+    if(!dp){
+        std::cout << "Memory allocation is impossible";
+        return 1;
+    }
+
+    dp -> setAge(5);
+    std::cout << "the value allcated at memory is "
+              << dp->getAge() <<".";
+    delete dp;
     return 0;
+
 }
+
+
+
