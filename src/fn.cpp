0eb2b86e724ee7b6bdf7c706236a24b1f7722587
sim <sim30217@gmail.com>
2023-09-27 22:52:51 +0900

    array object dynamic allocation

--- src/fn.cpp
+++ src/fn.cpp
@@ -14,21 +14,27 @@ int Dog::getAge(){
     return age;
 }
 void Dog::setAge(int a){
-    int age = a;
+    age = a;
 }
 int main()
 {
     Dog *dp;
-    dp = new Dog;
+    dp = new Dog[10];
     if(!dp){
         std::cout << "Memory allocation is impossible";
         return 1;
     }
+    for(int i=0; i<10; i++)
+    {
+        dp[i].setAge(i);
+    } 
+    for(int i=0; i<10; i++) 
+    {
+            std::cout << i << "th age of object is" <<
+            dp[i].getAge() << " ." << std::endl;
+    }
 
-    dp -> setAge(5);
-    std::cout << "the value allcated at memory is "
-              << dp->getAge() <<".";
-    delete dp;
+    delete[] dp;
     return 0;
 
 }
