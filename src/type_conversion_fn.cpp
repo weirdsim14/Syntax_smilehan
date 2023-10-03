65887463be87841ad8fae353c388ef40fcab1d16
sim <sim30217@gmail.com>
2023-10-03 23:53:41 +0900

    type conversion function

--- src/type_conversion_fn.cpp
+++ src/type_conversion_fn.cpp
@@ -1,9 +1,11 @@
-// Imagine you have a Temperature class that holds a temperature in Celsius and you want to get the temperature in Fahrenheit without doing the math manually every time. Here's how you could use a conversion operator to achieve that:
-
-
+/* Imagine you have a Temperature class that holds a temperature in Celsius
+and you want to get the temperature in Fahrenheit without doing the math manually every time.
+Here's how you could use a conversion operator to achieve that:
+ */
 #include <iostream>
 
-class Temperature {
+class Temperature
+{
 private:
     float celsius;
 
@@ -11,14 +13,16 @@ public:
     Temperature(float c) : celsius(c) {}
 
     // Conversion function to float (Fahrenheit)
-    operator float() {
-        return (celsius * 9.0/5.0) + 32;
+    operator float()
+    {
+        return (celsius * 9.0 / 5.0) + 32;
     }
 };
 
-int main() {
-    Temperature temp(25.0); // 25°C
-    float inFahrenheit = temp;  // Uses conversion function
+int main()
+{
+    Temperature temp(25.0);    // 25°C
+    float inFahrenheit = temp; // Uses conversion function
 
     std::cout << "Temperature in Fahrenheit: " << inFahrenheit << std::endl;
     return 0;
